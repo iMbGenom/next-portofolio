71cad9e0797246308c1e3d62ee96415dc7f616d2
sakata <radiliadi@gmail.com>
2019-08-01 22:27:31 +0700

    Done 2.26 - Get Portofolio Id

--- pages/portofolio.js
+++ pages/portofolio.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 import BaseLayout from '../components/layouts/BaseLayout'
 import { withRouter } from 'next/router'
+import axios from 'axios'
 
 class Portofolio extends Component {
 
+    static async getInitialProps({ query }) {
+        console.log(query)
+        const portofolioId = query.id
+        let portofolio = {}
+
+        try {
+            const response = await axios.get('http://localhost:3001/v/1/content?_id=' + portofolioId)
+            portofolio = response.data
+        } catch (err) {
+            console.log(err)
+        }
+
+        return {
+            portofolio
+        }
+    }
+
     render() {
+        const { portofolio } = this.props
+
         return (
             <BaseLayout>
                 <h1>I am Portofolio Page with param:</h1>
-                <h2>{this.props.router.query.id}</h2>
+                {/* <h2>{this.props.router.query.id}</h2> */}
+                <h1> {portofolio.data[0].Slug} </h1>
             </BaseLayout>
         )
     }
