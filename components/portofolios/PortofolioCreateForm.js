e3d7b24da29917bd29b479a1d77157fa713563d1
sakata <radiliadi@gmail.com>
2019-08-05 03:19:12 +0700

    Done 5.04 - More Input Types

--- components/portofolios/PortofolioCreateForm.js
+++ components/portofolios/PortofolioCreateForm.js
@@ -2,33 +2,67 @@ import React, { Component } from 'react'
 
 class PortofolioCreateForm extends Component {
     constructor(props) {
-      super(props);
-      this.state = {title: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        super(props)
+        this.state = {
+            title: '',
+            description: '',
+            language: ''
+        }
+    
+        this.handleChange = this.handleChange.bind(this)
+        // this.handleChangeDesc = this.handleChangeDesc.bind(this)
+        // this.handleChangeLang = this.handleChangeLang.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
   
     handleChange(event) {
-      this.setState({title: event.target.value});
+        const field = event.target.name
+        this.setState({
+            [field]: event.target.value
+        })
     }
+
+    // handleChangeDesc(event) {
+    //     this.setState({
+    //         description: event.target.value
+    //     })
+    // }
+
+    // handleChangeLang(event) {
+    //     this.setState({
+    //         language: event.target.value
+    //     })
+    // }
   
     handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.title);
-      event.preventDefault();
+        alert('A name was submitted: ' + this.state.title + ' ' + this.state.description + ' ' + this.state.language)
+        event.preventDefault()
     }
   
     render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      );
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input name="title" type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Description:
+                    <textarea name="description" value={this.state.description} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Pick your favorite Programming Language:
+                    <select name="language" value={this.state.language} onChange={this.handleChange}>
+                        <option value="javascript">Javascript</option>
+                        <option value="java">Java</option>
+                        <option value="c++">C++</option>
+                        <option value="c#">C#</option>
+                    </select>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
     }
-  }
+}
 
 export default PortofolioCreateForm
