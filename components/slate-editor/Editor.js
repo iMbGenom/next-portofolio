82d94cf1c9267a00a50c4d9b2f4d39a50bb3faaf
sakata <radiliadi@gmail.com>
2019-09-11 14:36:06 +0700

    Done 6.4 - Event Handler in Slate

--- components/slate-editor/Editor.js
+++ components/slate-editor/Editor.js
@@ -35,7 +35,22 @@ class SlateEditor extends Component {
   
     // On change, update the app's React state with the new editor value.
     onChange = ({ value }) => {
-      this.setState({ value })
+        this.setState({
+            value
+        })
+    }
+
+    // Define a new handler which prints the key that was pressed.
+    onKeyDown = (event, editor, next) => {
+        // Return with no changes if the keypress is not '&'
+        if (event.key !== '&') return next()
+
+        // Prevent the ampersand character from being inserted.
+        event.preventDefault()
+
+        // Change the value by inserting 'and' at the cursor's position.
+        editor.insertText('and')
+        return true
     }
   
     // Render the editor.
@@ -44,7 +59,11 @@ class SlateEditor extends Component {
         return (
             <Fragment>
                 { isLoaded &&
-                    <Editor value={this.state.value} onChange={this.onChange} />
+                    <Editor
+                        value={this.state.value}
+                        onChange={this.onChange}
+                        onKeyDown={this.onKeyDown}
+                    />
                 }
             </Fragment>
         )
