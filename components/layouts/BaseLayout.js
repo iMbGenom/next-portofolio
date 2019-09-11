e902e63d1c04c4023e787f600b40a3d4c169c476
sakata <radiliadi@gmail.com>
2019-09-11 12:24:31 +0700

    Done 6.1 - Blog Listing

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,22 +1,28 @@
 import React, { Fragment } from 'react'
 import Header from '../shared/Header'
+import Head from 'next/head'
 
 const BaseLayout = (props) => {
     const { className, children } = props
     const headerType = props.headerType || 'default'
 
     return (
-        <div className="layout-container">
-            {/* { headerType === 'index' && <Header className="port-nav-index" />} */}
-            {/* { headerType === 'default' && <Header className="port-nav-default" />} */}
-            <Header className={`port-nav-${headerType}`} />
-            <main className={`cover ${className}`}>
-                <div className="wrapper">
-                {children}
-                </div>
-            </main>
-        </div>
-
+        <Fragment>
+            <Head>
+                <title>Ulasan Gue</title>
+                <script src="https://kit.fontawesome.com/380d351f8f.js"></script>
+            </Head>
+            <div className="layout-container">
+                {/* { headerType === 'index' && <Header className="port-nav-index" />} */}
+                {/* { headerType === 'default' && <Header className="port-nav-default" />} */}
+                <Header className={`port-nav-${headerType}`} />
+                <main className={`cover ${className}`}>
+                    <div className="wrapper">
+                    {children}
+                    </div>
+                </main>
+            </div>
+        </Fragment>
     )
 
     // return (
