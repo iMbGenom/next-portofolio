99a10d7cc7c1ce073745345238a1e511b6b999e5
sakata <radiliadi@gmail.com>
2019-10-14 12:38:23 +0700

    9.04 - Robots.txt

--- server/index.js
+++ server/index.js
@@ -8,10 +8,20 @@ const port = parseInt(process.env.PORT, 10) || 4000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = routes.getRequestHandler(app)
+const robotsOptions = {
+    root: path.join(__dirname, '../static'),
+    headers: {
+        'Content-Type': 'text/plain;charset=UTF-8'
+    }
+}
 
 app.prepare().then(() => {
     const server = express()
     server.use(favicon(path.join(__dirname, '../static/images', 'favicon.ico')))
+
+    server.get('/robots.txt', (req, res) => {
+        return res.status(200).sendFile('robots.txt', robotsOptions)
+    })
 
     // server.get('/portofolio/:id', (req, res) => {
     //     console.log('------PORTOFOLIO-----')
