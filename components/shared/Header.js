10b80d25b6a4de655857a19188f1687bc92b725c
sakata <radiliadi@gmail.com>
2019-10-14 10:09:46 +0700

    8.10 - Dropdown on mobile fixed

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -146,9 +146,13 @@ export default class Example extends React.Component {
 
   render() {
     const { className } = this.props
+    const { isOpen } = this.state
+
+    const menuOpenClass = isOpen ? 'menu-open' : 'menu-close'
+
     return (
       <div>
-        <Navbar className={`port-navbar port-nav-base absolute ${className}`} color="transparent" dark expand="md">
+        <Navbar className={`port-navbar port-nav-base absolute ${className} ${menuOpenClass}`} color="transparent" dark expand="md">
           <NavbarBrand className="port-navbar-brand" href="/">Sakata Gintoki</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
